209562c1771ffbf3605ef6530e6b18cc32fc5988
AlexanderHauss <33121820+AlexanderHauss@users.noreply.github.com>
2018-06-13 16:05:56 +0300

    Create index.php

---
+++ work/errea/index.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0" />
+
+<head>
+			<meta http-equiv="X-UA-Compatible" content="IE=edge">
+			<!--[if lt IE 9]>
+			  <script src="html5shiv.min.js"></script>
+			  <script src="respond.min.js"></script>
+			<![endif]-->
+			<link rel="icon" type="image/png" href="https://olgalaut.diamonds/images/favicon.png">
+            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+            
+            <title>EARRINGS | O!LAUT</title>
+            <meta name="description" content="Renewing a 30 years old leader business through creative direction, print & web design.">
+            <!-------------CSS------------->  
+            <link rel="stylesheet" href="https://olgalaut.diamonds/css/reboot.css">
+<link rel="stylesheet" href="https://olgalaut.diamonds/css/master.css">
+<link rel="stylesheet" href="https://olgalaut.diamonds/css/grid12.css">
+<link rel="stylesheet" href="https://use.typekit.net/ogn7prs.css">
+<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+<meta name="format-detection" content="telephone=no">
+<script type="text/javascript" src="https://olgalaut.diamonds/js/jquery.js"></script>
+<script type="text/javascript" src="https://olgalaut.diamonds/js/pace.min.js"></script>
+<script type="text/javascript" src="https://olgalaut.diamonds/js/script.js"></script>
+			<script src="https://olgalaut.diamonds/js/bodymovin.js"></script>
+	
+            
+</head>
+<body>
+            <!----------HEAD INC------------>
+            <div id="loader"></div>
+
+<div id="warning">
+	<p>This website is optimized fo vertical navigation.<br>
+	Please turn me!</p>
+	<img src="https://olgalaut.diamonds/images/phone.svg">
+</div>
+
+<div id="pointer">
+	<img src="https://olgalaut.diamonds/images/mini-arrow.png">
+</div>
+
+<div class="cookie-bar">
+	This website use cookies / <a href="https://olgalaut.diamonds/cookie-policy/">Learn more</a>
+</div>
+
+<header>
+	<a class="home-link" href="https://olgalaut.diamonds/">
+		<div class="logo">
+			<img class="b" src="https://olgalaut.diamonds/images/logo-b.svg">
+			<img class="lend" src="https://olgalaut.diamonds/images/logo-lend.svg">
+		</div>
+	</a>
+	<div class="disable-trigger"></div>
+	<div class="menu-trigger">
+		<button class="hamburger hamburger--collapse" type="button">
+		  <span class="hamburger-box">
+			<span class="hamburger-inner"></span>
+		  </span>
+		</button>
+	</div>
+	<div class="back">
+		<img src="https://olgalaut.diamonds/images/arrow-back.svg">
+	</div>
+</header>
+
+<div class="nav-container">
+	<div class="overlay-container container cf">
+		<div class="menu">
+			<li><a class="link1" href="index.html">Home</a></li>
+			<li><a class="link2" href="index.html">Shop</a></li>
+			<li><a class="link3" href="index.html">Locations and Contacts</a></li>
+			<li><a class="link4" href="cookie-policy/index.html">Cookie Policy</a></li>
+		</div>
+		<div class="contact">
+			<div class="contact-inner dati">
+			<div class="mini-title">Olga Laut Joyellery</div>
+			<p>OFFICIAL RESOURCES<br>WEB<br>olgalaut.com<br>Olgalaut.diamonds<br>MARKETING<br><a href="mailto:pr@olgalaut.com">pr@olgalaut.com</a><br>ENGAGEMENT<br><a href="mailto:sales@olgalaut.com">sales@olgalaut.com</a><br>CLIENTS SERVISES<br>support@olga laut.com<br><a href="mailto:support@olgalaut.com">support@olgalaut.com</a>
+			</p>
+			</div>
+			<div class="contact-inner social">
+			<div class="mini-title">Get In touch</div>
+			<li><a target="_blank" href="https://www.instagram.com/o.laut_joyellery/">Instagram</a></li>
+			<li><a target="_blank" href="https://www.facebook.com/olautjoyellery">Facebook</a></li>
+			</div>
+		</div>
+		
+		<div class="main-progress-bar">
+			<div class="main-progress"></div>
+		</div>
+		
+		
+	</div>
+</div>
+
+
+
+            <!------------------------------>
+            
+                <!----------CONTENT------------->
+                
+                
+                
+                <div class="main-container single-proj">
+						
+					<section id="errea-section" class="cf work-section-single" data-anim="errea_anim" data-data="errea-data" data-bg="#e2e2e2">
+				  	 <div class="work-container">
+						  <div class="anim sx">
+							<div class="img-container"><img class="img" src="https://olgalaut.diamonds/images/home/errea.png" alt=""></div>
+							<div id="errea-data"></div>
+						  </div>
+						  <div class="work-txt">
+								<h2>
+								  E A R R I N G S<br />
+								  COLLECTION
+								</h2>
+								  <p>
+									 Black & white, lavender, deep & vivid green Jade, in 18k white, pink & yellow gold and in platinum earrings.<br>
+											<b>Stones:</b> Diamonds, multicolor, orange & mixed sapphires, black pearl, pink opals, Iranian turquoise, lapis lazuli, carved tanzanite,<br> 
+											onyx, emeralds, tourmalines, paraiba, amethyst, rubies, spinel, turqoise.
+								  </p>
+						  </div>
+					   </div>
+					</section>
+						
+						<div class="project-content">
+							
+							<section class="pad-extra">
+								<div class="container in-text">
+									<div class="in-text-item">
+										<div class="mini-container">
+											<h2 class="in-text-item text-center">E A R R I N G S</h2>
+										</div>
+										<div class="mini-container text-center">
+											<p><small>Olga Laut’s errings collection had a tradicional classic styles. Individual design will allow to emphasize feature of your style. The collection is made in a single copy of rare stones to find that only Olga Laut.</small>
+											</p>
+										</div>
+									</div>
+								</div>
+							</section>
+							
+							<section>
+								<div class="full-container">
+									<img class="full-img" src="https://olgalaut.diamonds/images/errea/basket.png">
+								</div>
+							</section>
+				
+						<!----------FOOTER INC------------>
+						<footer>
+	<div class="footer-inner container cf">
+
+		<div class="col-sm-3 col-md-3">
+			<img src="images/logo-blend.svg">
+		</div>
+		<div class="col-sm-3 col-md-3 mini-menu">
+				<li><a href="index.html">Home</a></li>
+				<li><a href="index.html">Shop</a></li>
+				<li><a href="index.html">Locations and Contacts</a></li>
+				<li><a href="cookie-policy/index.html">Cookie Policy</a></li>
+		</div>
+		<div class="col-sm-3 col-md-3 contact-inner">
+			<div class="mini-title">Olga Laut Joyellery</div>
+					<p>
+					OFFICIAL RESOURCES<br>
+					WEB<br>
+					olgalaut.com<br>
+					Olgalaut.diamonds<br>
+					MARKETING<br>
+					<a href="mailto:pr@olgalaut.com">pr@olgalaut.com</a><br>
+					ENGAGEMENT<br>
+					<a href="mailto:sales@olgalaut.com">sales@olgalaut.com</a><br>
+					CLIENTS SERVISES<br>
+					support@olga laut.com<br>
+					<a href="mailto:support@olgalaut.com">support@olgalaut.com</a>
+					</p>
+					</div>
+					<div class="contact-inner social">
+					<div class="mini-title">Get In touch</div>
+					<li><a target="_blank" href="https://www.instagram.com/o.laut_joyellery/">Instagram</a></li>
+					<li><a target="_blank" href="https://www.facebook.com/olautjoyellery">Facebook</a></li>
+		</div>
+	</div>
+</footer>						<!------------------------------>
+						
+						
+						
+						</div>
+                </div>
+
+              
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.3/TweenMax.min.js"></script>
+			<script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/ScrollMagic.js"></script>
+			<script src="https://olgalaut.diamonds/js/animation.gsap.js"></script>
+			<script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/debug.addIndicators.min.js"></script>
+			<script src="https://olgalaut.diamonds/js/work.js"></script>
+
+</body>
+
+
+</html>
+© 2018 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+API
+Training
+Shop
+Blog
+About
